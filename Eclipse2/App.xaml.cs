8b20d445091971a1e3058b7ba249e67a34fdb952
MoonLover69 <leenkahn@gmail.com>
2015-06-06 15:30:00 -0700

    Initial commit
    Adding very important files

---
+++ Eclipse2/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Eclipse2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SplashScreen ss = new SplashScreen("SplashScreen.png");
+            ss.Show(false);
+
+            Thread.Sleep(3000);
+
+            ss.Close(new TimeSpan());
+        }
+    }
+}
